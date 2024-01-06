9f21ef45050a0cf663b20397682d4f2577a4441f
Ali Darehshori <37948611+ali80da@users.noreply.github.com>
2024-01-06 10:49:42 +0330

    The Early Version of the Program
    none one

---
+++ Fibonachi/Fibonachi/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Author: MEZONE */
+
+/// <summary>
+/// 
+/// </summary>
+static int Fibonachi(int N)
+{
+    if (N == 1)
+        return (1);
+    if (N == 2)
+        return (1);
+    return (Fibonachi(N - 2) + Fibonachi(N - 1));
+}
+
+/*string Q;*/
+int Count = 1, X, i;
+
+Console.Write("\nEnter Number:");
+X = int.Parse(Console.ReadLine());
+Console.Write("Result is:\n");
+for (i = 1; i <= X; i++)
+    Console.Write("Fibonachi({01}): {00} \n",Fibonachi(i), Count++);
+Console.WriteLine("------------\nReceved ({0}) Fibonachi Number Complated.", X);
+
+/* HELLO FROM ER-A-SE, THIS PROGRAM CREATED BY ALI DAREHSHORI, FOR ALL PEOPLE IN THE WORLD, NO EXEPTION */
+Console.ReadKey();
+
+
+// Q = Console.ReadLine();
+// Console.Write("\nYou Want to Contniuo? "); // Y/N
+// if (Q == true)
+// {
+
+// }
+
+
+
+
+/// <summary>
+/// 
+/// </summary>
